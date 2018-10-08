6903c2268577f6fc22c48dddf6e0ee4c1a525d40
Parker Mclain <parkermclain94@gmail.com>
2018-10-07 22:39:13 -0400

    Final

--- App.py
+++ App.py
@@ -16,6 +16,7 @@ weights = [[], [], []]
 # Average female weight (US): 168.4 lb
 # https://en.wikipedia.org/wiki/Human_body_weight (2011 - 2014)
 
+
 # *******************
 # Generates random data and writes to text file
 # *******************
@@ -44,8 +45,8 @@ def generate_data():
 def main():
     # Uncomment if need to generate new dataset
     # generate_data()
-    # begin_learning(True, 0.25)
-    begin_learning(True, 0.75)
+    begin_learning(True, 0.25)
+    # begin_learning(True, 0.75)
 
     # begin_learning(False, 0.25)
     # begin_learning(False, 0.75)
@@ -54,29 +55,25 @@ def main():
 # *******************
 # Function to set up and begin learning process
 # *******************
-def begin_learning(hard, sample_fraction):
+def begin_learning(hard, fraction):
 
     normalized_data = normalize_data(pd.read_csv("SampleData.txt", header=None))
 
-    training_data = normalized_data.sample(frac=sample_fraction)
+    training_data = normalized_data.sample(frac=fraction)
 
     testing_data = normalized_data[~normalized_data.isin(training_data)]
 
-    # random_separation = [random.uniform(-0.1, 0.1), random.uniform(-0.1, 0.1), random.uniform(-0.1, 0.1)]
-    random_separation = [1, -1, 0.5]
-
-    original_separation = random_separation
+    random_separation = [random.uniform(-0.1, 0.1), random.uniform(-0.1, 0.1), random.uniform(-0.1, 0.1)]
+    # random_separation = [1, -1, 0.5]
 
     plt.figure(1)
-    final_separation = learn(training_data, testing_data, random_separation, 10, hard)
-    plot_male_and_females(normalized_data)
-    # plot_separation(original_separation, normalized_data, color="green")
-    # plot_separation(random_separation, normalized_data, color="blue")
+    final_separation = learning_algorithm(1000, training_data, testing_data, random_separation, hard)
+    plot_graphs(normalized_data)
     plt.show()
 
     plt.figure(2)
-    plot_male_and_females(normalized_data)
-    plot_separation(final_separation, normalized_data, color="red")
+    plot_graphs(normalized_data)
+    plot_separation(normalized_data, final_separation, color="red")
     plt.axis((0, 1, 0, 1))
     plt.show()
 
@@ -95,24 +92,23 @@ def normalize_data(all_data):
     return normalized_data
 
 
-def plot_male_and_females(data_frame):
+def plot_graphs(data_frame):
     males = data_frame[data_frame[2] == 0]
     females = data_frame[data_frame[2] == 1]
 
-    male_x = males[0]
-    male_y = males[1]
+    male_height = males[0]
+    male_weight = males[1]
 
-    female_x = females[0]
-    female_y = females[1]
+    female_height = females[0]
+    female_weight = females[1]
 
-    male = plt.scatter(male_x, male_y, s=50, c=np.full(males[2].shape, 'orange'), alpha=0.1)
-    female = plt.scatter(female_x, female_y, s=50, c=np.full(females[2].shape, 'blue'), alpha=0.1)
-
-    plt.legend((male, female), ('Male', 'Female'), scatterpoints=1, loc='lower left', ncol=3, fontsize=6)
+    male_graph = plt.scatter(male_height, male_weight, c=np.full(males[2].shape, 'orange'), alpha=0.1,  s=50)
+    female_graph = plt.scatter(female_height, female_weight, c=np.full(females[2].shape, 'blue'), alpha=0.1, s=50)
 
     plt.title("Male & Female - Weight vs Height")
     plt.xlabel("Height (ft)")
     plt.ylabel("Weight (lbs)")
+    plt.legend((male_graph, female_graph), ('Male', 'Female'), loc='upper right', ncol=3, fontsize=6, scatterpoints=1)
 
     return plt
 
@@ -136,148 +132,100 @@ def print_errors_and_weights():
     print("----------------------------------------")
 
 
-
-
-def plot_separation(separation_line, data, color="0.18"):
+def plot_separation(data, separation_line, color="0.15"):
+    bias = separation_line[2]
     weight_x = separation_line[0]
     weight_y = separation_line[1]
-    bias = separation_line[2]
 
     center = (((data[0].max() - data[0].min()) / 2) + data[0].min())
 
-    # Formula: y_weight = x_weight + bias
-    # Y = (x _weight / a) * y_weight + (bias / y_weight)
-    y1 = -(((weight_x * data[0].min()) / weight_y) + (bias / weight_y))
-    y2 = -(((weight_x * data[0].max()) / weight_y) + (bias / weight_y))
-    y_mid = -(((weight_x * center) / weight_y) + (bias / weight_y))
+    y_center = -(((weight_x * center) / weight_y) + (bias / weight_y))
+    y1 = -((bias / weight_y) + ((weight_x * data[0].min()) / weight_y))
+    y2 = -((bias / weight_y) + ((weight_x * data[0].max()) / weight_y))
 
-    ax = plt.axes()
-    ax.arrow(center, y_mid, 0.05, 0.05, head_width=0.025, head_length=0.025, fc='k', ec='k', color="b")
+    direction = plt.axes()
+    direction.arrow(center, y_center, 0.05, 0.05, fc='k', ec='k', color="blue", head_width=0.03, head_length=0.03)
 
     plt.plot([data[0].min(), data[0].max()], [y1, y2], color=color)
-    plt.axis((0,1,0,1))
+    plt.axis((0, 1, 0, 1))
     return plt
 
 
-def calculate_weight_after_delta_d(current_weight, current_pattern, hard_activation=True, alpha=alpha, k=0.5):
-    net = (current_weight[0] * current_pattern[0] +
-           current_weight[1] * current_pattern[1] +
-           current_weight[2])
+def calculate_new_weight(weight, number, hard=True):
+    net = ((weight[0] * number[0]) + (weight[1] * number[1]) + weight[2])
 
-    if hard_activation:
-        output = 1 if net > 0 else 0
-        delta_d = alpha * (current_pattern[2] - output)
-    else:
-        output = (np.tanh(net * k) + 1) / 2
-        delta_d = alpha * (current_pattern[2] - output)
-
-    current_pattern[0] *= delta_d
-    current_pattern[1] *= delta_d
-    current_pattern[2] = delta_d
-
-    current_weight[0] += current_pattern[0]
-    current_weight[1] += current_pattern[1]
-    current_weight[2] += current_pattern[2]
-
-    return current_weight
-
-
-def learn(train_df, test_df, sep_line, number_of_iterations, hard):
-    final_sep_line = None
-
-    for i in range(0, number_of_iterations):
-        print("iteration", i)
-
-        total_error = calculate_error(test_df, sep_line)
-        total_errors.append(total_error)
-
-        plot_separation(sep_line, test_df, color=str(i / number_of_iterations))
-
-        err = calculate_error(train_df, sep_line)
-        errors.append(err)
-
-        weights[0].append(sep_line[0])
-        weights[1].append(sep_line[1])
-        weights[2].append(sep_line[2])
-
-        if total_error < 0.00005:
-            break
-
-        # mix up the test data frame so that we learn in different ways(?)
-        train_df = train_df.sample(frac=1)
-        # For each element in the data_frame `train_df`
-        for index, row in train_df.iterrows():
-            new_weights = calculate_weight_after_delta_d(sep_line, row, hard_activation=hard)
-
-            sep_line[0] = new_weights[0]
-            sep_line[1] = new_weights[1]
-            sep_line[2] = new_weights[2]
-
-        final_sep_line = sep_line
-
-    return final_sep_line
-
-
-def calculate_error(data_frame, sep_line):
-    error_matrix = get_confusion_matrix(data_frame, sep_line)
-    return 1 - ((error_matrix[1] + error_matrix[0]) / (data_frame[0].count()))
-
-
-def get_output_for_row(current_pattern, current_weight):
-    net = (current_weight[0] * current_pattern[0] +
-           current_weight[1] * current_pattern[1] +
-           current_weight[2])
-
-    return 1 if net > 0 else 0
-
-
-def get_confusion_matrix(data_frame, sep_line):
-    true_positive = 0
-    true_negative = 0
-    false_positive = 0
-    false_negative = 0
-
-    for row in data_frame.iterrows():
-        r = row[1]
-
-        if len(sep_line) == 3:
-            gender = r[2]
-
-            if get_output_for_row(r, sep_line):
-                if gender == 1:
-                    true_positive += 1
-                else:
-                    false_positive += 1
-            else:
-                if gender == 0:
-                    true_negative += 1
-                else:
-                    false_negative += 1
+    if hard:
+        if net > 0:
+            result = 1
         else:
-            height = r[0]
-            weight = r[1]
-            gender = r[2]
-            x_weight = sep_line[0]
-            bias = sep_line[1]
+            result = 0
+        delta = (number[2] - result) * 0.1
+    else:
+        result = (1 + np.tanh(net * 0.5)) / 2
+        delta = (number[2] - result) * 0.1
 
-            # 0 <= bx - c
-            net = x_weight * height - bias * 1
+    number[0] = number[0] * delta
+    number[1] = number[1] * delta
+    number[2] = delta
 
-            if net < 0:
-                if gender == 1:
-                    true_positive += 1
-                else:
-                    false_positive += 1
+    weight[0] = weight[0] + number[0]
+    weight[1] = weight[1] + number[1]
+    weight[2] = weight[2] + number[2]
+
+    return weight
+
+
+def learning_algorithm(iterations, train_data, test_data, separation_input, hard):
+    final = None
+
+    for i in range(0, iterations):
+        print("iteration", i)
+        error1 = get_error(test_data, separation_input)
+        total_errors.append(1 - ((error1[1] + error1[0]) / (test_data[0].count())))
+        if total_errors[i] < 0.00005:
+            break
+        plot_separation(test_data, separation_input, color=str(i / iterations))
+
+        error2 = get_error(train_data, separation_input)
+        errors.append(1 - ((error2[1] + error2[0]) / (train_data[0].count())))
+
+        weights[0].append(separation_input[0])
+        weights[1].append(separation_input[1])
+        weights[2].append(separation_input[2])
+
+        train_data = train_data.sample(frac=1)
+
+        for x, row in train_data.iterrows():
+            learned_weight = calculate_new_weight(separation_input, row, hard=hard)
+
+            separation_input[0] = learned_weight[0]
+            separation_input[1] = learned_weight[1]
+            separation_input[2] = learned_weight[2]
+
+        final = separation_input
+
+    return final
+
+
+def get_error(data_matrix, separation_matrix):
+    FP = 0
+    FN = 0
+    TP = 0
+    TN = 0
+    for x in data_matrix.iterrows():
+        temp = x[1]
+        if ((separation_matrix[0] * temp[0]) + (separation_matrix[1] * temp[1]) + separation_matrix[2]) > 0:
+            if temp[2] == 1:
+                TP = TP + 1
             else:
-                if gender == 0:
-                    true_negative += 1
-                else:
-                    false_negative += 1
+                FP = FP + 1
+        else:
+            if temp[2] == 0:
+                TN = TN + 1
+            else:
+                FN = FN + 1
 
-    return (true_positive,
-            true_negative,
-            false_positive,
-            false_negative)
+    return TP, TN, FP, FN
 
 
 if __name__ == "__main__":
